e67b84a4d87327dedd123340bab04926b3e22a2f
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-17 11:55:39 +0700

    $nin ไม่มีค่าอยู่ในอาร์เรย์ รวมทั้งไม่มีฟิลด์นั้นรวมอยู่ด้วย

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 $nin ไม่มีค่าอยู่ในอาร์เรย์ รวมทั้งไม่มีฟิลด์นั้นรวมอยู่ด้วย 
 เช่น เราจะสืบค้นด็อกคิวเมนต์ที่ฟิลด์ field มีค่าไม่ตรงกับค่าใดค่าหนึ่งใน ["programming", "hardware"] รวมทั้งไม่มีฟิลด์ field
 
+data
+{
+    "_id": "key",
+    "name": "test",
+    "age": 46,
+    "field": ["programming", "hardware"]
+}
+
 */
 db.writer.find({ field: { $nin: ["programming", "hardware"] } });
