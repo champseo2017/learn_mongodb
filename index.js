dfcd8eb05dbb25c9ea1ad992e55e8226cca4fad1
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-22 08:55:08 +0700

    โมเดลโครงสร้างต้นไม้  การอ้างอิงด้วยบรรพบุรุษ (Array of Ancestors)

--- index.js
+++ index.js
@@ -2,20 +2,44 @@
 
 โมเดลโครงสร้างต้นไม้
 
-การอ้างอิงด้วยลูก (Child References)
- - เพิ่มด๊อกคิวเมนต์ในคอลเล็กชัน books โดยอ้างอิงโหนดลูกที่อยู่ภายใต้โหนดแม่ในฟิลด์ child
+การอ้างอิงด้วยบรรพบุรุษ (Array of Ancestors)
+ - เก็บเส้นทางไว้ในโหนดเด็ก child ว่าสืบเชื้อสายมาจากใครบบ้าง
 
 */
+
+// เราจะเพิ่มด็อกคิวเมนต์ในคอลเล็กชัน books ที่เก็บเชื้อสายบรรพบุรุษเป็นอาร์เรย์ไว้ในฟิลด์ family และ เก็บหมวดหมู่พ่อแม่ไว้ในฟิลด์ cat
+db.books.update({ _id: "books" }, { $set: { family: [] } });
+db.books.update({ _id: "office" }, { $set: { family: ["books"] } });
+db.books.update({ _id: "graphic" }, { $set: { family: ["books"] } });
+db.books.update({ _id: "programming" }, { $set: { family: ["books"] } });
+db.books.update(
+  { _id: "publishing" },
+  { $set: { family: ["books", "graphic"] } }
+);
+db.books.update(
+  { _id: "multimedia" },
+  { $set: { family: ["books", "graphic"] } }
+);
+db.books.update(
+  { _id: "photoshop" },
+  { $set: { family: ["books", "graphic", "publishing"] } }
+);
+db.books.update(
+  { _id: "illustrator" },
+  { $set: { family: ["books", "graphic", "publishing"] } }
+);
 db.books.update(
   { _id: "indesign" },
-  { $set: { child: [] } }
+  { $set: { family: ["books", "graphic", "publishing"] } }
 );
 
-// สืบค้นเพื่อดูข้อมูลของลูกที่อยู่ภายใต้โหนดพ่อแม่ เช่น
-db.books.findOne({_id: "publishing"}).child
+// เราสามารถดูเส้นทางการสืบเชื้อสายของข้อมูลที่สืบค้นได้ว่ามาจากไหน
+db.books.findOne({ _id: "photoshop" }).family
+// แสดงเส้นทางการสืบเชื้อสายหรือโครงสร้างลำดับชั้น
 
-// เราสามารถกำหนดให้ฟิลด์ child เป็นอินเด็กซ์ที่ใช้อ้างอิงข้อมูลโดยกำหนดค่าเป็น 1 ซึ้งจะช่วยให้ค้นหาข้อมูลด้วยโหนดลูกได้เร็วขึ้น
-db.books.createIndex({ child: 1 })
+// เราสามารถกำหนดให้ฟิลด์ family เป็นอินเด็กซ์ที่ใช้อ้างอิงข้อมูลโดยกำหนดค่าเป็น 1 ซึ้งจะช่วยให้ค้นหาข้อมูลด้วยโหนดบรรพบุรุษได้เร็วขึ้น
+db.books.createIndex({ family: 1 })
 
-// สืบค้นด๊อกคิวเมนต์พ่อแม่ที่อ้างอิงโหนดลูกในฟิลด์ child
-db.books.find({ child: "photoshop" })
+// เราสามารถสืบค้นด็อกคิวเมนต์ลูกหลานที่อ้างอิงโหนดบรรพบุรุษในฟิลด์ family ได้อย่างรวดเร็ว เช่น สืบค้นด๊อกคิวเมนต์ลูกหลานที่อยู่ภายใต้ โหนด family ฟิลด์ graphic
+db.books.find({ family: "graphic" })
+// ด็อกคิวเมนต์หนังสือที่อยู่ภายใต้ graphic
