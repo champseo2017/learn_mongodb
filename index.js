3ecf3402d8fc1512b17438f9cf000af1ae2a64f0
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-04-21 09:36:21 +0700

    ประเภทของอินเด็กซ์ใน MongoDB
    อินเด็กซ์ที่ไม่ซ้ำกัน (Unique Indexes)

--- index.js
+++ index.js
@@ -9,46 +9,26 @@ MongoDB
 
 /* 
 
-อินเด็กซ์ข้อความ (Text Indexes)
-เป็นการค้นหาเนื้อหาสตริงที่อยู่ในคอเล็กชัน การจัดทำอินเด็กซ์จะไม่จัดเก็บคำหยุด (Stop words) คือ คำที่พบบ่อยที่สุด เช่น "the", "a", "or" และ คำที่มีรากฐานมาจากคำศัพท์เดียวกัน อินเด็กซ์จะจัดเก็บเฉพาะคำหลักเท่านั้น
+อินเด็กซ์ที่ไม่ซ้ำกัน (Unique Indexes)
+ฟิลด์ที่จัดทำอินเด็กซ์จะไม่จัดเก็บค่าที่ซ้ำ
 
 รูปแบบ
-db.collection.createIndex({ field: "text" })
+db.collection.createIndex(<key: index type>, { unique: true })
+
 
 */
 
-// ตัวอย่าง เราจะจัดเก็บอินเด็กซ์ไว้บนฟิลด์ subject
-db.workshop.insertMany([
-  {
-    subject: "basic web",
-    teacher: "piya",
-  },
-  {
-    subject: "web programming",
-    teacher: "sira",
-  },
-  {
-    subject: "web design",
-    teacher: "sira",
-  },
-  {
-    subject: "program development",
-    teacher: "piya",
-  },
-  {
-    subject: "web master",
-    teacher: "nara",
-  },
-]);
+// ตัวอย่างบนฟิลด์เดียว ต้องการกำหนดคุณสมบัติอินเด็กซ์ที่ไม่ซ้ำกันบนฟิลด์ name ของ
+// คอลเล็กชัน customer
+db.customer.createIndex({ name: 1 }, { unique: true });
 
-// สร้างอินเด็กซ์ข้อความไว้บนฟิลด์ subject
-db.workshop.createIndex({ subject: "text" })
-
-// สืบค้นข้อความที่ทำ Index โดยสืบค้นคำว่า "program" ที่มีอยู่ในคอลเล็กชัน workshop
-db.workshop.find({
-  $text: {
-    $search: "program"
+// อินเด็กซ์ที่ไม่ซ้ำกันให้กับอินเด็กซ์ผสม {name: 1, age: -1}
+db.customer.createIndex(
+  {
+    name: 1,
+    age: -1,
+  },
+  {
+    unique: true,
   }
-})
-
-// ใช้ตัวดำเนินการ $text จะใช้อินเด็กซ์ข้อความช่วยในการค้นหาข้อความ และ ตัวดำเนินการ $search ใช้สืบค้นด้วยคำที่ระบุ คือ "program" จะได้ ด็อกคิวเมนต์ 2 รายการที่มีหัวเรื่องที่เกี่ยวกับคำว่า "program" คือ program development และ web programming
+);
